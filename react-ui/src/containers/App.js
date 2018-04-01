b371ad9016ad36912b80aa4e3decb097318e74a9
amirhosseinnazari <hnirsnazari@yahoo.com>
2018-04-01 12:45:09 +0430

    first commit

---
+++ react-ui/src/containers/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import classes from './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: null,
+      fetching: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        this.setState({
+          message: json.message,
+          fetching: false
+        });
+      }).catch(e => {
+        this.setState({
+          message: `API call failed: ${e}`,
+          fetching: false
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div className={classes.App}>
+        <div className={classes.App_header}>
+          <img src={logo} className={classes.App_logo} alt="logo" />
+          <h2>ROC Test page</h2>
+        </div>
+        <p className={classes.App_intro}>
+          {'This is '}
+          <a href="https://github.com/mars/heroku-cra-node">
+            {'create-react-app with a custom Node/Express server'}
+          </a><br/>
+        </p>
+        <p className={classes.App_intro}>
+          {this.state.fetching
+            ? 'Fetching message from API'
+            : this.state.message}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
